30fcfd124f958f52b95132130d16fc382ffc6ae7
Ricardo <ricantony66@gmail.com>
2023-03-30 17:02:19 -0300

    add function onclick

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
     <div className="App">
       <div className='app_videos'>
        <Video />
+       <Video />
+       <Video />
+       <Video />
+
       </div>
     </div>
   );
--- src/pages/video.js
+++ src/pages/video.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "./video.css"
 
-function video() {
+function Video() {
+
+    const videoRef = useRef(null)
+    const [play, setPlay] = useState(false)
+
+        function handdleStart(){
+            
+            if(play){
+                videoRef.current.pause()
+                setPlay(false)
+            } else {
+                videoRef.current.play()
+                setPlay(true)
+            }
+        }
+
+
+
   return (
     <div className='video'>
-        video
-        <video className='video__player'>
-            
+        <video 
+            className='video__player'
+            ref={videoRef}
+            onClick={handdleStart}
+            loop
+            src='https://firebasestorage.googleapis.com/v0/b/jornada3-a7924.appspot.com/o/bird.mp4?alt=media&token=c683dc6f-f027-43e1-a9ba-43be147e4843'
+
+        >
         </video>
 
     </div>
   )
 }
 
-export default video
+export default Video
