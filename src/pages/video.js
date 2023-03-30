c97c808364ea740e48e4b24ccc28e903a280261a
Ricardo <ricantony66@gmail.com>
2023-03-30 16:06:25 -0300

    add page video.js e video.css

---
+++ src/pages/video.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./video.css"
+
+function video() {
+  return (
+    <div className='video'>
+        video
+        <video className='video__player'>
+            
+        </video>
+
+    </div>
+  )
+}
+
+export default video
