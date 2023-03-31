57c9e697128856d3ccdc853ff8a4bea618b11c11
Ricardo <ricantony66@gmail.com>
2023-03-31 17:25:37 -0300

    add img do vinil e estilizando vinil  com animacao

--- src/pages/componetes/footer/videofooter.js
+++ src/pages/componetes/footer/videofooter.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import "./videofooter.css"
+import React from "react";
+import "./videofooter.css";
 
 function Videofooter() {
   return (
-    <div className='videofooter'>
-        <div className='videofooter__text'>
+    <div className="videofooter">
+      <div className="videofooter__text">
         <h3>@Ricardo Antonio</h3>
         <p>Descrição do video</p>
-        <p>icone</p>
-        <p>titulo da musica</p>
+        <div className="videofooter__music">
+          <p>icone</p>
+          <p>titulo da musica</p>
         </div>
-       
+      </div>
+      <img
+        className="videofooter__record"
+        alt="Imagem de vinil girando"
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/vinil.png"
+      />
     </div>
-  )
+  );
 }
 
-export default Videofooter
+export default Videofooter;
