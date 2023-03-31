f865f92b56d78f98ab06145d167b35316cf74359
Ricardo <ricantony66@gmail.com>
2023-03-31 17:05:19 -0300

    import videofooter.css e add descricoes

---
+++ src/pages/componetes/footer/videofooter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./videofooter.css"
+
+function Videofooter() {
+  return (
+    <div className='videofooter'>
+        <div className='videofooter__text'>
+        <h3>@Ricardo Antonio</h3>
+        <p>Descrição do video</p>
+        <p>icone</p>
+        <p>titulo da musica</p>
+        </div>
+       
+    </div>
+  )
+}
+
+export default Videofooter
